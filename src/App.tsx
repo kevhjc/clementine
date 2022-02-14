811f41ec4a70f3f795153f56fd964758fd9cbc53
Kevin Chung <kevin.c@me.com>
2022-02-14 14:40:17 -0800

    Update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Intro from './components/Intro';
+import LearnMore from './components/LearnMore';
 // import Container from './components/Container';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Intro />
-      {/* <Container /> */}
+    <div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Intro />} />
+          <Route path='learn-more' element={<LearnMore />} />
+          {/* <Container /> */}
+        </Route>
+      </Routes>
+
       <Footer />
-    </>
+    </div>
   );
 }
 

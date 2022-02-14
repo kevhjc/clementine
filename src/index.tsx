5825eb845fcc86ed4da1c1eb2e8925d2b14acdaa
Kevin Chung <kevin.c@me.com>
2022-02-13 22:50:09 -0800

    Add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './tailwind.css';
-
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);

cb39abc1c131d25ef82f8d0b9dc2834d0c1295f5
Kevin Chung <kevin.c@me.com>
2022-02-14 14:40:35 -0800

    Add component

---
+++ src/components/LearnMore.tsx
@@ -0,0 +1,22 @@
+export default function LearnMore() {
+  return (
+    <div className='px-4 font-mono mt-36'>
+      <div className='max-w-7xl mx-auto px-10'>
+        <div className='text-center'>
+          <p className='mt-2 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
+            Welcome to your <span className='font-black'>Canvas.</span>
+          </p>
+          <p className='mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
+            A place to save your thoughts and ideas
+          </p>
+          <p className='mt-24 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
+            What's the stack?
+          </p>
+          <p className='mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
+            A place to save your thoughts and ideas
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

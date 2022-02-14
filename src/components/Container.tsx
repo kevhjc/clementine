d9c36d032f13d6923d27dff0d8eec31e41b0412e
Kevin Chung <kevin.c@me.com>
2022-02-14 00:17:37 -0800

    Add container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,16 @@
 export default function Container() {
-  return <div></div>;
+  return (
+    <div className='px-4 font-mono mt-48'>
+      <div className='max-w-7xl mx-auto px-10'>
+        <div className='text-center'>
+          <p className='mt-2 text-3xl leading-8 font-medium tracking-tight text-gray-900 sm:text-5xl'>
+            Say 👋 to <span className='font-black'>Canvas.</span>
+          </p>
+          <p className='mt-4 max-w-2xl text-xl text-gray-500 mx-auto'>
+            A place to save your thoughts and ideas
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }

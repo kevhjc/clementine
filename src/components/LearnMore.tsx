3b1323d112d03a5bbf258d03c2b7ecdc29265510
Kevin Chung <kevin.c@me.com>
2022-02-14 15:01:39 -0800

    Update info

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -4,16 +4,32 @@ export default function LearnMore() {
       <div className='max-w-7xl mx-auto px-10'>
         <div className='text-center'>
           <p className='mt-2 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
-            Welcome to your <span className='font-black'>Canvas.</span>
+            What's <span className='font-black'>Canvas?</span>
           </p>
-          <p className='mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
-            A place to save your thoughts and ideas
+          <p className='font-sans mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
+            A place to save your thoughts and ideas. Save your notes, links, or
+            images/videos to your Canvas.
           </p>
-          <p className='mt-24 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
+          <p className='mt-20 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
             What's the stack?
           </p>
-          <p className='mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
-            A place to save your thoughts and ideas
+          <p className='font-sans mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
+            React.js, TypeScript, Tailwind CSS
+          </p>
+          <p className='mt-20 text-3xl leading-8 font-medium tracking-tight sm:text-5xl'>
+            Who made it?
+          </p>
+          <p className='font-sans mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto'>
+            Built by{' '}
+            <a
+              href='https://www.kevc.xyz/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline hover:no-underline'
+            >
+              {`Kevin`}
+            </a>
+            {' ✌️'}
           </p>
         </div>
       </div>

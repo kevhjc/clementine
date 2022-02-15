a09a58661e5eb1c85504c345e2d7525d83380c5a
Kevin Chung <kevin.c@me.com>
2022-02-14 20:20:30 -0800

    Add home route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Intro from './components/Intro';
+import Home from './components/Home';
 import LearnMore from './components/LearnMore';
 // import Container from './components/Container';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Header />}>
           <Route index element={<Intro />} />
+          <Route path='home' element={<Home />} />
           <Route path='learn-more' element={<LearnMore />} />
           {/* <Container /> */}
         </Route>

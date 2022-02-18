7193cbbd324aeb7fcaa51fef0091dbee6e848481
Kevin Chung <kevin.c@me.com>
2022-02-18 12:09:39 -0800

    refactor: rename components and endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,17 @@ import { supabase } from './supabaseClient';
 
 import { UserContext } from './context/UserContext';
 import CommandPalette from './components/CommandPalette';
-import Header from './components/Header';
+import Navigation from './components/Navigation';
 import Intro from './components/Intro';
+import Auth from './components/Auth';
 import Home from './components/Home';
 import LearnMore from './components/LearnMore';
 // import Footer from './components/Footer';
 
 function App() {
-  const [session, setSession] = useState(null);
+  const [session, setSession] = useState(supabase.auth.session());
 
   useEffect(() => {
-    setSession(supabase.auth.session());
-
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
@@ -25,8 +24,9 @@ function App() {
     <UserContext.Provider value={session}>
       <CommandPalette>
         <Routes>
-          <Route path="/" element={<Header />}>
+          <Route path="/" element={<Navigation />}>
             <Route index element={<Intro />} />
+            <Route path="signin" element={<Auth />} />
             <Route path="home" element={<Home />} />
             <Route path="learn-more" element={<LearnMore />} />
           </Route>

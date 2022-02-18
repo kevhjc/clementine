bd92dd28642e89525ee6dd4afaee166275cbc4ca
Kevin Chung <kevin.c@me.com>
2022-02-18 12:08:09 -0800

    refactor(helpers): create utils folder

---
+++ src/utils/helpers.ts
@@ -0,0 +1,21 @@
+import faker from '@faker-js/faker';
+
+export const reorder = <T>(
+  list: T[],
+  startIndex: number,
+  endIndex: number
+): T[] => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+export const getItems = (count: number) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `Item ${k + 1}`,
+    primary: faker.commerce.productDescription(),
+    secondary: faker.date.past().toString(),
+    department: faker.commerce.productMaterial(),
+  }));

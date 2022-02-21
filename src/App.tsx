4d2c555072cacc7146bd8affe9b39fca0fa83116
Kevin Chung <kevin.c@me.com>
2022-02-21 00:36:38 -0800

    feat: add user context

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 
 import { UserContext } from './context/UserContext';
+import { SessionContext } from './context/SessionContext';
 import CommandPalette from './components/CommandPalette';
 import Navigation from './components/Navigation';
 import Intro from './components/Intro';
@@ -12,32 +13,37 @@ import LearnMore from './components/LearnMore';
 import NotFound from './components/NotFound';
 
 export default function App() {
+  const [user, setUser] = useState(supabase.auth.user());
   const [session, setSession] = useState(supabase.auth.session());
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((_event, session) => {
+      console.log(`Supbase auth event: ${_event}`);
       setSession(session);
+      setUser(session?.user ?? null);
     });
-  }, [session]);
+  }, [user, session]);
 
   return (
-    <UserContext.Provider value={session}>
-      <CommandPalette>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Intro />} />
-            <Route path="signin" element={<Auth />} />
-            {session ? (
-              <Route path="home" element={<Home />} />
-            ) : (
-              <Route path="home" element={<Auth />} />
-            )}
-            <Route path="/category/:id" />
-            <Route path="learn-more" element={<LearnMore />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </CommandPalette>
-    </UserContext.Provider>
+    <SessionContext.Provider value={session}>
+      <UserContext.Provider value={user}>
+        <CommandPalette>
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route index element={<Intro />} />
+              <Route path="signin" element={<Auth />} />
+              {session ? (
+                <Route path="home" element={<Home />} />
+              ) : (
+                <Route path="home" element={<Auth />} />
+              )}
+              <Route path="/category/:id" />
+              <Route path="learn-more" element={<LearnMore />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </CommandPalette>
+      </UserContext.Provider>
+    </SessionContext.Provider>
   );
 }

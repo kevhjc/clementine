70d331dcabdbc902d6e42f0ce9a24bad656484cd
Kevin Chung <kevin.c@me.com>
2022-02-22 13:24:05 -0800

    fix: update entry conditional rendering

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -113,7 +113,7 @@ const Home = () => {
             </div>
           </div>
           <ul>
-            {userEntries > 0 ? (
+            {userEntries ? (
               <div className="rounded-md bg-neutral-50/50 p-3 dark:bg-neutral-800/50">
                 <EntryList items={entries} deleteEntryById={deleteEntryById} />
               </div>

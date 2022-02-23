cf074c30fc7f7ebca2cb1690a2d6b846ad7e220a
Kevin Chung <kevin.c@me.com>
2022-02-23 11:31:03 -0800

    style: update checkbox alignment

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -26,7 +26,7 @@ const TaskItem = ({ task, onDelete }) => {
         onChange={toggleComplete}
         checked={isComplete ? true : ''}
       />
-      <div className="mr-2 flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-md border-[1.5px] border-neutral-800/70 bg-transparent focus-within:border-sky-400 dark:border-neutral-400">
+      <div className="mr-2 mt-[1px] flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-md border-[1.5px] border-neutral-800/70 bg-transparent focus-within:border-sky-400 dark:border-neutral-400">
         <svg
           className="pointer-events-none hidden h-3 w-3 fill-current"
           version="1.1"

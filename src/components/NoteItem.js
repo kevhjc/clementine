49aeb08ac082162aebbc89f69822f97a7adf2934
Kevin Chung <kevin.c@me.com>
2022-02-24 11:12:45 -0800

    style: update entry button styling

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,7 +6,7 @@ const NoteItem = ({ note, onDelete }) => {
       </span>
       <div className="absolute right-3 z-50 justify-center">
         <button
-          className="hidden flex-shrink-0 rounded bg-neutral-200 px-2 pb-0.5 transition-all duration-75 ease-in-out hover:bg-red-600 hover:text-white group-hover:block dark:bg-neutral-600"
+          className="hidden flex-shrink-0 rounded border border-neutral-300 bg-white px-2 pb-0.5 text-red-500 transition-all duration-75 ease-in-out hover:bg-neutral-100 group-hover:block dark:border-neutral-500 dark:bg-neutral-700 dark:hover:bg-neutral-600"
           aria-hidden="true"
           onClick={(e) => {
             e.preventDefault();

7d780fa1b3e44cbca8ed3d4e5caea3b33ea7e468
Kevin Chung <kevin.c@me.com>
2022-03-04 21:08:20 -0800

    style: update command icon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -20,11 +20,11 @@ export default function Navigation() {
         <div className="ml-6 flex basis-1/12">
           {session ? (
             <a href="/home">
-              <img src={Logo} alt="Logo" width={35} height={35} />
+              <img src={Logo} alt="Logo" width={40} height={40} />
             </a>
           ) : (
             <a href="/">
-              <img src={Logo} alt="Logo" width={30} height={30} />
+              <img src={Logo} alt="Logo" width={40} height={40} />
             </a>
           )}
         </div>
@@ -34,14 +34,18 @@ export default function Navigation() {
             className="mr-6 rounded-md bg-neutral-200/40 p-2 outline-none transition-colors duration-100 ease-in-out hover:bg-neutral-200 hover:text-neutral-900 dark:bg-neutral-800/50 dark:hover:bg-neutral-700 dark:hover:text-neutral-100"
             onClick={handleMenuClick}
           >
-            <svg viewBox="0 0 60 60" className="h-5 w-5 fill-current">
-              <path
-                d="M49,38h-7V22h7c6.065,0,11-4.935,11-11S55.065,0,49,0S38,4.935,38,11v7H22v-7c0-6.065-4.935-11-11-11S0,4.935,0,11
-	s4.935,11,11,11h7v16h-7C4.935,38,0,42.935,0,49s4.935,11,11,11s11-4.935,11-11v-7h16v7c0,6.065,4.935,11,11,11s11-4.935,11-11
-	S55.065,38,49,38z M42,11c0-3.859,3.14-7,7-7s7,3.141,7,7s-3.14,7-7,7h-7V11z M11,18c-3.86,0-7-3.141-7-7s3.14-7,7-7s7,3.141,7,7v7
-	H11z M18,49c0,3.859-3.14,7-7,7s-7-3.141-7-7s3.14-7,7-7h7V49z M22,38V22h16v16H22z M49,56c-3.86,0-7-3.141-7-7v-7h7
-	c3.86,0,7,3.141,7,7S52.86,56,49,56z"
-              />
+            <svg
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              shapeRendering="geometricPrecision"
+            >
+              <path d="M18 3a3 3 0 00-3 3v12a3 3 0 003 3 3 3 0 003-3 3 3 0 00-3-3H6a3 3 0 00-3 3 3 3 0 003 3 3 3 0 003-3V6a3 3 0 00-3-3 3 3 0 00-3 3 3 3 0 003 3h12a3 3 0 003-3 3 3 0 00-3-3z" />
             </svg>
           </button>
         </div>
